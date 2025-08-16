7794730c4c8051d832252a184ed6088c3231c3d5
qureshil20824-collab <qureshil20824@gmail.com>
2025-08-17 00:42:41 +0530

    Add files via upload

---
+++ task1_math_operations.py
@@ -0,0 +1,15 @@
+# Task 1: Performing Basic Mathematical Operations
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nAddition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
